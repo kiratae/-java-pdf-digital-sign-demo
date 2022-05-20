96766a8f729d9794f7eb2472b54795bbc6c57366
tanaphon.k [PC] <tanaporn.k@clicknext.com>
2022-05-20 17:08:37 +0700

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,114 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.cert.Certificate;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfAnnotation;
+import com.itextpdf.text.pdf.PdfAppearance;
+import com.itextpdf.text.pdf.PdfFormField;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.security.BouncyCastleDigest;
+import com.itextpdf.text.pdf.security.DigestAlgorithms;
+import com.itextpdf.text.pdf.security.ExternalDigest;
+import com.itextpdf.text.pdf.security.ExternalSignature;
+import com.itextpdf.text.pdf.security.MakeSignature;
+import com.itextpdf.text.pdf.security.PrivateKeySignature;
+import com.itextpdf.text.pdf.security.MakeSignature.CryptoStandard;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static final String KEYSTORE = "C:\\my\\cert\\OilStock\\TanapornKleaklom.pfx";
+    public static final char[] PASSWORD = "password".toCharArray();
+    public static final String SRC = "C:\\my\\temp\\hello.pdf";
+    public static final String DEST = "C:\\my\\temp\\hello_signed%s.pdf";
+    public static final String SIGNAME = "Tanaporn";
+
+    public static void main(String[] args) throws GeneralSecurityException, IOException, DocumentException {
+        System.out.println("Hello World!");
+        BouncyCastleProvider provider = new BouncyCastleProvider();
+        Security.addProvider(provider);
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        ks.load(new FileInputStream(KEYSTORE), PASSWORD);
+        String alias = (String) ks.aliases().nextElement();
+        PrivateKey pk = (PrivateKey) ks.getKey(alias, PASSWORD);
+        Certificate[] chain = ks.getCertificateChain(alias);
+        createPdf(SRC);
+        sign(SRC, SIGNAME, String.format(DEST, 1), chain, pk, DigestAlgorithms.SHA256,
+                provider.getName(), CryptoStandard.CMS, "Test 1", "TH");
+    }
+
+    public static void sign(String src, String fieldName, String dest, Certificate[] chain,
+            PrivateKey pk, String digestAlgorithm, String provider,
+            CryptoStandard subfilter, String reason, String location)
+            throws GeneralSecurityException, IOException, DocumentException {
+        // Creating the reader and the stamper
+        PdfReader reader = new PdfReader(src);
+        FileOutputStream os = new FileOutputStream(dest);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        // Creating the appearance
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        appearance.setReason(reason); // optional
+        appearance.setLocation(location); // optional
+        appearance.setVisibleSignature(fieldName);
+        // Creating the signature
+        ExternalSignature pks = new PrivateKeySignature(pk, digestAlgorithm, provider);
+        ExternalDigest digest = new BouncyCastleDigest();
+        MakeSignature.signDetached(appearance, digest, pks, chain,
+                null, null, null, 0, subfilter);
+    }
+
+    public static void createPdf(String filename) throws IOException, DocumentException {
+        // step 1: Create a Document
+        Document document = new Document();
+        // step 2: Create a PdfWriter
+        PdfWriter writer = PdfWriter.getInstance(
+                document, new FileOutputStream(filename));
+        // step 3: Open the Document
+        document.open();
+        // step 4: Add content
+        document.add(new Paragraph("Hello World!"));
+        // create a signature form field
+        PdfFormField field = PdfFormField.createSignature(writer);
+        field.setFieldName(SIGNAME);
+        // set the widget properties
+        field.setPage();
+        field.setWidget(new Rectangle(72, 732, 144, 780), PdfAnnotation.HIGHLIGHT_INVERT);
+        field.setFlags(PdfAnnotation.FLAGS_PRINT);
+        // add it as an annotation
+        writer.addAnnotation(field);
+        // maybe you want to define an appearance
+        PdfAppearance tp = PdfAppearance.createAppearance(writer, 72, 48);
+        tp.setColorStroke(BaseColor.BLUE);
+        tp.setColorFill(BaseColor.LIGHT_GRAY);
+        tp.rectangle(0.5f, 0.5f, 71.5f, 47.5f);
+        tp.fillStroke();
+        tp.setColorFill(BaseColor.BLUE);
+        ColumnText.showTextAligned(tp, Element.ALIGN_CENTER,
+                new Phrase("SIGN HERE"), 36, 24, 25);
+        field.setAppearance(PdfAnnotation.APPEARANCE_NORMAL, tp);
+        // step 5: Close the Document
+        document.close();
+    }
+}
